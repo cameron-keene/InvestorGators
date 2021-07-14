612a99fb7c3d701ede437528fe67f8961b381f33
andrewjfreeman29 <ajfreeman29@gmail.com>
2021-07-14 18:37:31 -0400

    updated search function

--- main.cpp
+++ main.cpp
@@ -80,26 +80,52 @@ DailyStock readStockData(string lineFile, string symbol, string name) {
 vector<DailyStock> Search(Date d1, Date d2, vector<DailyStock> fullArr) {
     vector <DailyStock> retVec;
     for (int i = 0; i < fullArr.size(); i++) {
+        // checks if in a valid year
         if (d1.GetYear() <= fullArr[i].GetDate().GetYear() && d2.GetYear() >= fullArr[i].GetDate().GetYear()) {
+            // checks if in one of the edge years
             if (d1.GetYear() == fullArr[i].GetDate().GetYear() || d2.GetYear() == fullArr[i].GetDate().GetYear()) {
+                //checks if d1 year is same as daily stock
                 if (d1.GetYear() == fullArr[i].GetDate().GetYear()) {
-                    if (d1.GetMonth() < fullArr[i].GetDate().GetMonth()) {
+                    //checks if d1 month is less than ds, (and if d2 month is greater than ds or if the end year is different) then adds ds
+                    if (d1.GetMonth() < fullArr[i].GetDate().GetMonth() && (d2.GetMonth() > fullArr[i].GetDate().GetMonth() || d2.GetYear() != fullArr[i].GetDate().GetYear())) {
                         retVec.push_back(fullArr[i]);
                     }
                     else {
-                        if (d1.GetMonth() == fullArr[i].GetDate().GetMonth()) {
+                        //checks if d1 month equals ds month and d2 month does not
+                        if (d1.GetMonth() == fullArr[i].GetDate().GetMonth() && d2.GetMonth() != fullArr[i].GetDate().GetMonth()) {
+                            // if d1 day is less than ds day adds ds
                             if (d1.GetDay() <= fullArr[i].GetDate().GetDay()) {
                                 retVec.push_back(fullArr[i]);
+                            }
+                        }
+                        // checks if d2 month equals ds month and d1 month does not
+                        else if (d1.GetMonth() != fullArr[i].GetDate().GetMonth() && d2.GetMonth() == fullArr[i].GetDate().GetMonth() && d1.GetYear() != fullArr[i].GetDate().GetYear()) {
+                            // if d2 day is greater than ds day adds ds
+                            if (d2.GetDay() >= fullArr[i].GetDate().GetDay()) {
+                                retVec.push_back(fullArr[i]);
+                            }
+                        }
+                        else {
+                            // if d1 month is less than or equal to ds month and d2 month is greater than or equal to ds month
+                            if (d1.GetMonth() <= fullArr[i].GetDate().GetMonth() && d2.GetMonth() >= fullArr[i].GetDate().GetMonth()) {
+                                // if d1 day is less than ds day and d2 day is greater than ds day adds ds
+                                if (d1.GetDay() <= fullArr[i].GetDate().GetDay() && d2.GetDay() >= fullArr[i].GetDate().GetDay()) {
+                                    retVec.push_back(fullArr[i]);
+                                }
                             }
                         }
                     }
                 }
                 else {
+                    //this executing means ds year equals d2 year but not d1 year
+                    // checks if d2 month is greater than ds month, if so adds ds
                     if (d2.GetMonth() > fullArr[i].GetDate().GetMonth()) {
                         retVec.push_back(fullArr[i]);
                     }
                     else {
+                        //checks if ds month equals d2 month 
                         if (d2.GetMonth() == fullArr[i].GetDate().GetMonth()) {
+                            // checks if d2 day is greater than or equal to ds day if so adds ds
                             if (d2.GetDay() >= fullArr[i].GetDate().GetDay()) {
                                 retVec.push_back(fullArr[i]);
                             }
@@ -107,6 +133,7 @@ vector<DailyStock> Search(Date d1, Date d2, vector<DailyStock> fullArr) {
                     }
                 }
             }
+            //else adds if in a middle year
             else {
                 retVec.push_back(fullArr[i]);
             }
@@ -148,22 +175,30 @@ int main(){
 
 
 
+
+
     //Fake date inputs, testing search function
-    Date d1h = Date("10/24/2018");
-    Date d2h = Date("12/12/2018");
+    Date d1h = Date("12/20/2018");
+    Date d2h = Date("12/25/2018");
 
     cout << d1h.GetMonth();
     cout << d2h.GetDay();
 
     vector<DailyStock> subVec = Search(d1h, d2h, dailyStocks);
 
+    cout << endl;
+    cout << endl;
+    cout << endl;
+
     cout << subVec.size();
 
+    cout << endl;
+    cout << endl;
     cout << endl;
 
     //Outputing results of search 
     for (int i = 0; i < subVec.size(); i++) {
-        cout << subVec[i].GetName() << "'s daily return's: " << subVec[i].GetDailyReturn();
+        cout << subVec[i].GetName() << "'s daily return's: " << subVec[i].GetDailyReturn() << "on: " << subVec[i].GetDate().GetMonth() << "/" << subVec[i].GetDate().GetDay() << "/" << subVec[i].GetDate().GetYear() << endl;
     }
 
 
