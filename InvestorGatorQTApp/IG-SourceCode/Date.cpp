5fbb2c97c57aa714be66983956b4b07a91ba5400
andrewjfreeman29 <ajfreeman29@gmail.com>
2021-07-28 16:30:10 -0400

    Added application source code

---
+++ InvestorGatorQTApp/IG-SourceCode/Date.cpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Date.h"
+#include <string>
+#include <sstream>
+
+Date::Date() {
+	this->month = 0;
+	this->day = 0;
+	this->year = 0;
+}
+
+Date::Date(std::string date) {
+	std::string strMonth, strDay, strYear;
+	std::istringstream sstring(date);
+
+	std::getline(sstring, strMonth, '/');
+	std::getline(sstring, strDay, '/');
+	std::getline(sstring, strYear);
+
+	this->month = (short) std::stoi(strMonth);
+	this->day = (short) std::stoi(strDay);
+	this->year = (short) std::stoi(strYear);
+}
+short Date::GetMonth() {
+	return month;
+}
+short Date::GetDay() {
+	return day;
+}
+short Date::GetYear() {
+	return year;
+}
---
+++ InvestorGatorQTApp/IGsourceCode/Date.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+class Date
+{
+	short month;
+	short day;
+	short year;
+
+public:
+	Date();
+	Date(std::string date);
+	short GetMonth();
+	short GetDay();
+	short GetYear();
+
+};
+
